05d84d35de8c4d492007422dc1d71f1868b4ae67
Chance Hudson <jchancehud@gmail.com>
2019-04-06 18:45:44 -0500

    Hello world

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const electron = require('electron')
+const { app, BrowserWindow } = electron
 
+const win = new BrowserWindow({
+  width: 800,
+  height: 600
+})
 
+app.on('ready', () => {
+  win.loadFile('index.html')
+})
