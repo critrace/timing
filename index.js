db50a15ca839ebed9a8c8cc0813446f09f01124a
Chance Hudson <jchancehud@gmail.com>
2019-04-07 10:13:50 -0500

    Add packaging system

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const electron = require('electron')
 const { app, BrowserWindow } = electron
+require('electron-debug')({
+  enabled: true,
+  devToolsMode: 'right',
+  showDevTools: false,
+})
 
 app.on('ready', () => {
   const win = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
   })
   win.loadFile('build/index.html')
 })
